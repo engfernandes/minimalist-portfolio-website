6132ecc45f192c05ef384b867f039f24fb16617d
Gabriel Fernandes <gabriel.fernandes.06@outlook.com>
2025-02-03 11:37:11 -0300

    :lipstick: ui: adding initial components

---
+++ src/components/ui/index.ts
@@ -0,0 +1,7 @@
+export * from "./button";
+export * from "./typography";
+export * from "./card";
+export * from "./navigation-menu";
+export * from "./scroll-area";
+export * from "./skeleton";
+export * from "./switch";
