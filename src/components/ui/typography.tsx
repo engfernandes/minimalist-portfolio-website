e3531009b777954442683fb07e1c4b1944852532
Gabriel Fernandes <gabriel.fernandes.06@outlook.com>
2025-02-03 21:28:15 -0300

    :lipstick: ui: fixing paragraph margin top style

--- src/components/ui/typography.tsx
+++ src/components/ui/typography.tsx
@@ -13,7 +13,7 @@ const typographyVariants = cva("", {
       h2: "scroll-m-20 border-b text-3xl leading-none font-semibold tracking-tight first:mt-0",
       h3: "scroll-m-20 text-2xl font-semibold tracking-tight",
       h4: "scroll-m-20 text-xl font-semibold tracking-tight",
-      p: "leading-7 [&:not(:first-child)]:mt-6",
+      p: "leading-7",
       blockquote: "mt-6 border-l-2 pl-6 italic",
       inlineCode:
         "relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold",
